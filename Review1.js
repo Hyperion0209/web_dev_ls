857023c2401b6159ba43377c0eb5a5ef7bfdc8b9
Hyperion0209 <101580076+Hyperion0209@users.noreply.github.com>
2022-08-25 23:50:32 +0530

    Add files via upload

---
+++ Review1.js
@@ -0,0 +1,21 @@
+const Review1 = () => {
+  return (
+    <div className="card" style={{color: "#000000" , width: "18rem" }}>
+  <div className="card-body">
+    <h5 className="card-title">Aryan Bhosale</h5>
+    <h6 className="card-subtitle mb-2 text-muted">Software Engineer</h6>
+    <p className="card-text">
+     What an amazing website.!!
+    </p>
+    <a href="#" className="card-link">
+      Card link
+    </a>
+    <a href="#" className="card-link">
+      Another link
+    </a>
+  </div>
+</div>
+  );
+}
+ 
+export default Review1;
